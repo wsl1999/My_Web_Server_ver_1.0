965f0887813eaa2951ec5eacaba98aaa53d2b550
wsl_starsky <wangshuailong1999@163.com>
2019-08-08 11:01:39 +0800

    terminal显示输出优化

--- main.cpp
+++ main.cpp
@@ -16,48 +16,48 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-	options opt;
-	opt_default_set(opt);
-	int opt_result = opt_response(argc, argv, opt);
-	if (opt_result != 0)
-	{
-		printf("Please input the command line arguments according to the usage!\n");
-		return 1;
-	}
-	printf("Welcome to my web server!\n");
+    options opt;
+    opt_default_set(opt);
+    int opt_result = opt_response(argc, argv, opt);
+    if (opt_result != 0)
+    {
+        printf("Please input the command line arguments according to the usage!\n");
+        return 1;
+    }
+    printf("Welcome to my web server!\n");
 
-	int sock;
-	struct sockaddr_in server_address;
-	bzero(&server_address, sizeof(server_address));
-	server_address.sin_family = PF_INET;
+    int sock;
+    struct sockaddr_in server_address;
+    bzero(&server_address, sizeof(server_address));
+    server_address.sin_family = PF_INET;
 
-	if (strcmp(opt.addr_str,"localhost")==0||!opt.addr_str)
-		server_address.sin_addr.s_addr = htons(INADDR_ANY);
-	else
-		server_address.sin_addr.s_addr = inet_addr(opt.addr_str);
-	server_address.sin_port = htons(opt.port);
+    if (strcmp(opt.addr_str, "localhost") == 0 || !opt.addr_str)
+        server_address.sin_addr.s_addr = htons(INADDR_ANY);
+    else
+        server_address.sin_addr.s_addr = inet_addr(opt.addr_str);
+    server_address.sin_port = htons(opt.port);
 
-	sock = socket(AF_INET, SOCK_STREAM, 0);
-	assert(sock >= 0);
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    assert(sock >= 0);
 
-	int ret = bind(sock, (struct sockaddr *)&server_address, sizeof(server_address));
-	assert(ret != -1);
+    int ret = bind(sock, (struct sockaddr *)&server_address, sizeof(server_address));
+    assert(ret != -1);
 
-	if (ret >= 0)
-		cout << "Bind success!Connect to\nhttp://" << opt.addr_str << ":" << opt.port << "/" << endl;
-	cout << "Please enter " << opt.addr_str << ":" << opt.port << " in your website address column." << endl;
+    if (ret >= 0)
+        cout << "Bind success!Connect to\nhttp://" << opt.addr_str << ":" << opt.port << "/" << endl;
+    cout << "Please enter " << opt.addr_str << ":" << opt.port << " in your website address column." << endl;
     printf("\nModules loading....\n");
-	module_load(); //load modules
-	ret = listen(sock, 8);
-	assert(ret != -1);
+    module_load(); //load modules
+    ret = listen(sock, 8);
+    assert(ret != -1);
 
-	processpool *pool = processpool::get(sock);
-	if (pool)
-	{
-		pool->run();
-		delete pool;
-	}
-	close(sock);
+    processpool *pool = processpool::get(sock);
+    if (pool)
+    {
+        pool->run();
+        delete pool;
+    }
+    close(sock);
 
-	return 0;
+    return 0;
 }
